b5778e057c64253af4dc8e52d4b68b2401cc4de3
aadya275 <aagarwal1_be25@thapar.edu>
2026-07-22 09:39:34 +0530

    Time: 0 ms (100%), Space: 10.5 MB (72.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+ void Combinations(vector<int>& nums,int idx,vector<vector<int>>&ans) 
+ {
+    int n=nums.size();
+    if(idx==n)
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=idx;i<n;i++)
+    {
+        swap(nums[idx],nums[i]);
+        Combinations(nums,idx+1,ans);
+        swap(nums[idx],nums[i]);
+    }
+ }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int idx=0;
+        Combinations(nums,idx,ans);
+        return ans;
+
+        
+    }
+};
